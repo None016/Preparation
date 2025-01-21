7dc340b18525bcb938b1195977e9bf7c0ac1f5b7
LAPTOP-88JQ3VC3\1 <Hiro0161@yandex.ru>
2025-01-21 15:58:06 +0500

    добавлена темная тема в index.html

--- statick/js/index.js
+++ statick/js/index.js
@@ -1,3 +1,10 @@
-const section_hero = document.getElementById("hero");
-section_hero.style.height = window.innerHeight - 50 + "px";
+// const section_hero = document.getElementById("hero");
+// section_hero.style.height = window.innerHeight - 50 + "px";
 
+const tumbler = document.getElementById("theme");
+
+const header = document.getElementById('header');
+
+tumbler.addEventListener('click', function (){
+    document.documentElement.setAttribute("theme", tumbler.checked ? 'dark': '')
+})
